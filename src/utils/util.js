e0bd206dc3e0084d4ade7404ad8275e0fa8398cb
choi-hye-rin <hr.choi@fastfive.co.kr>
2023-09-06 17:16:00 +0900

    fix: random id 생성 util로 보내기

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,6 +1,12 @@
+import dayjs from "dayjs";
+
 export default {
   getCategoryLabel(categoryList, id) {
     const label = categoryList.filter((cate) => cate.id === id)[0]?.value;
     return label;
+  },
+  createRandomId() {
+    const id = `${Math.random()}${dayjs().format("YYMMDDHHmmss")}`;
+    return id;
   },
 };
