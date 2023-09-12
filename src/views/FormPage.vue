5805e93b39a66b0c09d7d6fddcb01dc0704a660e
choi-hye-rin <hr.choi@fastfive.co.kr>
2023-09-12 18:21:17 +0900

    refactor: setup 사용해보기

--- src/views/FormPage.vue
+++ src/views/FormPage.vue
@@ -6,6 +6,9 @@
       <div v-for="(value, key) in formData" :key="key">
         {{ key }} >> {{ value }}
       </div>
+      <div v-for="(value, key) in computedFormData" :key="key">
+        {{ key }} >> {{ value }}
+      </div>
     </div>
     <div class="wrapper">
       <div v-for="msg in errorMsg" :key="msg" class="error">
@@ -15,6 +18,7 @@
   </div>
 </template>
 <script>
+import { ref, computed } from "vue";
 import UserForm from "../components/formPage/userForm.vue";
 
 export default {
@@ -22,19 +26,47 @@ export default {
   components: {
     UserForm,
   },
-  data() {
-    return {
-      formData: {},
-      errorMsg: {},
+  // data() {
+  //   return {
+  //     formData: {},
+  //     errorMsg: {},
+  //   };
+  // },
+  // methods: {
+  //   updateFormData: function (formValue, errorMsg) {
+  //     this.formData = formValue;
+  //     this.errorMsg = errorMsg;
+  //   },
+  // },
+  // computed: {
+  //   computedFormData() {
+  //     const data = {};
+  //     for (const [key, value] of Object.entries(this.formData)) {
+  //       data[key] = `값을 변경하는 과정... → ${value}`;
+  //     }
+  //     return data;
+  //   },
+  // },
+  setup() {
+    const formData = ref({});
+    const errorMsg = ref({});
+
+    const updateFormData = (formValue, error) => {
+      formData.value = formValue;
+      errorMsg.value = error;
     };
+
+    const computedFormData = computed(() => {
+      const data = {};
+
+      for (const [key, value] of Object.entries(formData.value)) {
+        data[key] = `값을 변경하는 과정... → ${value}`;
+      }
+      return data;
+    });
+
+    return { formData, errorMsg, updateFormData, computedFormData };
   },
-  methods: {
-    updateFormData: function (formValue, errorMsg) {
-      this.formData = formValue;
-      this.errorMsg = errorMsg;
-    },
-  },
-  computed: {},
 };
 </script>
 <style lang="scss" scoped>
